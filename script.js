07befad776041fdf89cba39dcb775be5d17bd25b
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2023-02-21 13:15:27 -0300

    Basic Structure

--- script.js
+++ script.js
@@ -1,59 +1,125 @@
-let canvas = document.getElementById("tela");
-let ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#FFFFFF";
+document.addEventListener("DOMContentLoaded", () =>{
 
-const w = 15;
-const h = w;
-let x = 15;
-let y = 0;
-
-
-//Math.random();
-
-function frames () {
-
-
-}
-
-
-function peça1 () {
-    y += 1;
-    let rotacao = []
-
-    ctx.clearRect(0, 0, 400, 600);
-    ctx.fillRect(x, y, w, h);
-    ctx.fillRect(x + 20, y, w, h);
-    ctx.fillRect(x + 40, y, w, h);
-    ctx.fillRect(x + 60, y, w, h);
-
+    const grid = document.querySelector(".grid")
+    let squares = Array.from(document.querySelectorAll(".grid div"))
+    const ScoreDisplay = document.querySelector("#score")
+    const StartBtn = document.querySelector("#start-button")
+    const width = '3.8vh'
+    
+    //Peças
+    
+    ``` Peça 1
+    O X X   O O O   O X O   O O O
+    O X O   X X X   O X O   X O O
+    O X O   O O X   X X O   X X X
     ```
-    OOOO
-    XXXX
-    OOOO
-    OOOO
+    
+    const lTetromino = [
+        [1, width+1, width*2+1,2],
+        [width, width+1, width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2]
+    ]
+    
+    ``` Peça 2
+    O O O   X O O   O O O   X O O
+    O X X   X X O   O X X   X X O
+    X X O   O X O   X X O   O X O
     ```
-
-    // ctx.clearRect(0, 0, 400, 600);
-    // ctx.fillRect(x, y, w, h);
-    // ctx.fillRect(x, y + 20, w, h);
-    // ctx.fillRect(x, y + 40, w, h);
-    // ctx.fillRect(x, y + 60, w, h);
-
-
-
-}
-
-
-function test() {
-    y += 1;
-    ctx.clearRect(0, 0, 400, 600);
-    ctx.fillRect(x, y, w, h);
-    ctx.fillRect(x + 20, y, w, h);
-    ctx.fillRect(x, y + 20, w, h);
-    ctx.fillRect(x, y + 40, w, h);
-}
-
-setInterval(test, 1);
-
-
+    
+    const sTetromino = [
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1]
+    ]
+    
+    ``` Peça 3
+    O O O   O X O   O O O   O X O
+    X X O   X X O   X X O   X X O
+    O X X   X O O   O X X   X O O
+    ```
+    
+    const zTetromino = [
+        [width,width+1,width*2+1,width*2+2],
+        [1,width,width+1,width*2],
+        [width,width+1,width*2+1,width*2+2],
+        [1,width,width+1,width*2]
+    ]
+    
+    ``` Peça 4
+    O X O   O X O   O O O   O X O
+    X X X   O X X   X X X   X X O
+    O O O   O X O   O X O   O X O
+    ```
+    
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+    
+    ``` Peça 5
+    X X O   X X O   X X O   X X O
+    X X O   X X O   X X O   X X O
+    O O O   O O O   O O O   O O O
+    ```
+    
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+    
+    ``` Peça 6
+    O X O O   O O O O   O X O O    O O O O
+    O X O O   X X X X   O X O O    X X X X
+    O X O O   O O O O   O X O O    O O O O
+    O X O O   O O O O   O X O O    O O O O
+    ```
+    
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+    
+    const tetrominoes = [lTetromino,sTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
+    
+    let currentPosition = 4
+    let current = tetrominoes[0][0]
+    
+    function draw() {
+        current.forEach(i => {
+            squares[currentPosition + i].classList.add('tetromino')
+        })
+    }
+    
+    draw()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    });
